cfc09df8b79361409430a000586c97686c2aa241
BadQuanta <BadQuanta@gmail.com>
2019-09-29 06:00:13 -0700

    Revamping server-side UI.

---
+++ bin/pixeldeck.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const pixeldeck = module.exports = require('../lib/root')
+pixeldeck.get(/\/(.*)\.ts\.svg/, require('../lib/getTilesetSvg'))
+pixeldeck.get(/\/(.*)\.tm\.svg/, require('../lib/getTilemapSvg'))
+pixeldeck.get('/', require('../lib/getRoot'))
+if (require.main === module) require('../lib/start')();
--- config/default.js
+++ config/default.js
@@ -1,9 +1,18 @@
 const Path = require('path')
+const package = require('../package')
 module.exports = {
+  name: package.name,
+  version: package.version,
   resDir: Path.resolve(__dirname, '../res'),
   viewsDir: Path.resolve(__dirname, '../views'),
   locals: {
     cache: false,
     compileDebug: true
+  },
+  dbName: `${package.name}.json`,
+  dbOpts: {
+    autoload: true,
+    autosave: true,
+    pretty: true
   }
 }
