80d3d275aae9ec01aec88edf205789a8bf77f638
BadQuanta <BadQuanta@gmail.com>
2019-09-29 22:29:37 -0700

    just renamed the express instance back to app, instead of root.

--- bin/pixeldeck.js
+++ bin/pixeldeck.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const pixeldeck = module.exports = require('../lib/root')
+const pixeldeck = module.exports = require('../lib/app')
 pixeldeck.get(/\/(.*)\.ts\.svg/, require('../lib/getTilesetSvg'))
 pixeldeck.get(/\/(.*)\.tm\.svg/, require('../lib/getTilemapSvg'))
 pixeldeck.get('/', require('../lib/getRoot'))
