383240af6f297e1874ff49f54c5bd38dfdd88022
BadQuanta <BadQuanta@gmail.com>
2019-09-29 22:32:27 -0700

    didn't need indexing in bin

--- bin/pixeldeck.js
+++ bin/pixeldeck.js
@@ -1,6 +1,2 @@
 #!/usr/bin/env node
-const pixeldeck = module.exports = require('../lib/app')
-pixeldeck.get(/\/(.*)\.ts\.svg/, require('../lib/getTilesetSvg'))
-pixeldeck.get(/\/(.*)\.tm\.svg/, require('../lib/getTilemapSvg'))
-pixeldeck.get('/', require('../lib/getRoot'))
 if (require.main === module) require('../lib/start')();
