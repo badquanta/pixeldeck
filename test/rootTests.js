ee87bb730c91f5a60d3bd1d1e84b294a214c8ba5
BadQuanta <BadQuanta@gmail.com>
2019-10-07 11:47:41 -0700

    TileSet handler now uses findOrLoad

--- test/rootTests.js
+++ test/rootTests.js
@@ -16,7 +16,11 @@ describe('pixeldeck/', function () {
   it('can give us a tileSet', function () {
     return test.agent.get('/tilesets/lpc-terrains/terrain-map-v7.ts.svg').expect(200)
   })
-  it('can give us a tilemap', function () {
+  it('can give us a tilemap svg', function () {
     return test.agent.get('/test/test1.tm.svg').expect(200)
+  })
+
+  it('can give us a tilemap html', function () {
+    return test.agent.get('/test/test1.tm.html').expect(200)
   })
 })
