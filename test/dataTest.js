c00dc3391bff4366dd2968076e63a49c79ff05d3
BadQuanta <BadQuanta@gmail.com>
2019-10-07 01:50:21 -0700

    Got to 100% coverage of TileSets via poor testing.

--- test/dataTest.js
+++ test/dataTest.js
@@ -19,4 +19,35 @@ describe('data', function () {
       test.expect(TileMaps.collection.count()).to.eql(1)
     })
   })
+
+  describe('TileSets', function () {
+    it('works', function () {
+      const TileSets = test.pixeldeck.data.TileSets
+      const set = TileSets.findOrLoad('/tilesets/lpc-terrains/terrain-v7.json')
+      test.expect(set).to.be.ok;
+      [
+        'columns', 'image', 'imageheight',
+        'imagewidth', 'margin', 'name',
+        'spacing', 'terrains', 'tilecount',
+        'tileheight', 'tiles', 'type',
+        'version'
+      ].forEach((key) => {
+        test.expect(set[key]).to.not.be.undefined
+      })
+    })
+
+    it('should save to db', function () {
+
+      const TileSets = test.pixeldeck.data.TileSets
+      test.expect(TileSets.collection.count()).to.eql(0)
+      const set = new TileSets({ path: '/one/two/three' })
+      test.expect(TileSets.collection.count()).to.eql(1)
+      const set2 = new TileSets({ path: '/one/two/three' })
+      test.expect(TileSets.collection.count()).to.eql(1)
+      test.expect(set).to.eql(set2)
+      const set3 = new TileSets({ path: '/one/two/four' })
+      test.expect(TileSets.collection.count()).to.eql(2)
+      test.expect(set3.type).to.be.undefined
+    })
+  })
 })
